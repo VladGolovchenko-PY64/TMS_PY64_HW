ba9d3f44187496561c69971f308704c15f34398b
VladGolovchenko-PY64 <vlad-head@mail.ru>
2025-06-23 22:45:47 +0300

    Add homework OOP 1 and 2(not finish)

---
+++ lesson_14.sql
@@ -0,0 +1,60 @@
+create database if not exists lesson14;
+
+use lesson14;
+
+show tables;
+
+create table users (
+	id int unsigned primary key auto_increment,
+	username varchar(64) not null unique,
+	password varchar(64) not null,
+	email varchar(64) not null
+);
+
+
+create table seller (
+	id int unsigned primary key auto_increment,
+	company varchar(64) not null,
+	phone varchar(64) not null
+);
+
+create table products (
+	id int unsigned primary key auto_increment,
+	name varchar(255),
+	cost int unsigned null,
+	count int unsigned null,
+	seller_id int unsigned not null,
+    foreign key (seller_id) references seller (id)
+);
+
+create table orders (
+	id int unsigned primary key auto_increment,
+	user_id int unsigned not null,
+	product_id int unsigned not null,
+	count int null,
+    foreign key (user_id) references users (id) on delete cascade,
+    foreign key (product_id) references products (id)
+);
+
+insert users(username, password, email) values ('vlad', '12345678', 'v@v.com');
+
+insert seller(company, phone) values('ibm', '+375 99 1234567');
+
+insert products(name, cost, count, seller_id) values ('phone', 123, 20, 1);
+
+insert orders(user_id, product_id, count) values (1, 1, 10);
+
+select * from users;
+select * from seller;
+select * from products;
+select * from orders;
+
+drop table users;
+drop table seller;
+drop table products;
+drop table orders;
+
+desc users;
+desc products;
+
+
